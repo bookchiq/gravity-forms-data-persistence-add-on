5e92dca4f33bc3e1ff4bd086378937bc84093b4e
asthait <asthait@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-20 05:39:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-data-persistence-add-on/trunk@601419 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ persistent_multipage_forms.php
@@ -0,0 +1,143 @@
+<?php
+/*
+  Plugin Name: Gravity Forms Data Persistence Add-On
+  Plugin URI: http://asthait.com
+  Description: This is an <a href="http://www.gravityforms.com/" target="_blank">Gravity Form</a> plugin. A big limitation with Gravity Form is, in case of big multipage forms, if you close or refresh the page during somewhere midle of some step. all the steps data will loose. this plugin solves that problem.
+  Author: Astha Team
+  Version: 1
+  Author URI: http://asthait.com
+ */
+
+
+
+add_action("gform_post_paging", "page_changed", 10, 3);
+
+function page_changed($form, $coming_from_page, $current_page) {
+    if ($form['isPersistent']) {
+        if (is_user_logged_in()) {
+            $option_key = getFormOptionKeyForGF($form);
+            update_option($option_key, json_encode($_POST));
+        }
+    }
+}
+
+add_filter("gform_pre_render", "pre_populate_the_form");
+
+function pre_populate_the_form($form) {
+    if ($form['isPersistent']) {
+        $option_key = getFormOptionKeyForGF($form);
+        if (get_option($option_key)) {
+            $_POST = json_decode(get_option($option_key), true);
+        }
+    }
+
+
+    return $form;
+}
+
+add_action("gform_post_submission", "set_post_content", 10, 2);
+
+function set_post_content($entry, $form) {
+    if ($form['isPersistent']) {
+        //Update form data in wp_options table
+        if (is_user_logged_in()) {
+            $option_key = getFormOptionKeyForGF($form);
+            update_option($option_key, json_encode($_POST));
+        }
+
+        //Update entry in wp_options table
+        $entry_option_key = getEntryOptionKeyForGF($entry);
+        if (get_option($entry_option_key)) {
+            //Delete old entry from GF tables
+            delete_entry_from_gf_tables(get_option($entry_option_key));
+        }
+        update_option($entry_option_key, $entry['id']);
+    }
+}
+
+function delete_entry_from_gf_tables($lead_id) {
+    global $wpdb;
+    $table_lead = $wpdb->prefix . "rg_lead";
+    $table_detail = $wpdb->prefix . "rg_lead_detail";
+    $table_detail_long = $wpdb->prefix . "rg_lead_detail_long";
+    $table_lead_meta = $wpdb->prefix . "rg_lead_meta";
+    $table_lead_notes = $wpdb->prefix . "rg_lead_notes";
+
+    $sql = "DELETE FROM $table_detail_long WHERE lead_detail_id in( SELECT id FROM $table_detail WHERE lead_id = $lead_id )";
+    $wpdb->query($sql);
+
+    $sql = "DELETE FROM $table_lead_notes WHERE  lead_id = $lead_id";
+    $wpdb->query($sql);
+
+    $sql = "DELETE FROM $table_lead_meta WHERE  lead_id = $lead_id";
+    $wpdb->query($sql);
+
+    $sql = "DELETE FROM $table_detail WHERE  lead_id = $lead_id";
+    $wpdb->query($sql);
+
+    $sql = "DELETE FROM $table_lead WHERE  id = $lead_id";
+    $wpdb->query($sql);
+}
+
+function getFormOptionKeyForGF($form) {
+
+    global $current_user;
+    get_currentuserinfo();
+
+    $option_key = $current_user->user_login . '_GF_' . $form['id'];
+
+    return $option_key;
+}
+
+function getEntryOptionKeyForGF($entry) {
+
+    global $current_user;
+    get_currentuserinfo();
+
+    $option_key = $current_user->user_login . '_GF_' . $form['id'] . '_entry';
+
+    return $option_key;
+}
+
+//Add persistent checkbox to the form settings
+add_action("gform_advanced_settings", "persistency_settings", 10, 2);
+
+function persistency_settings($position, $form_id) {
+
+    //create settings on position 50 (right after Admin Label)
+    if ($position == 600) {
+        ?>
+        <li>
+            <input type="checkbox" id="form_persist_value" onclick="SetFormPersistency();" /> Enable form persistence
+            <label for="form_persist_value">              
+        <?php gform_tooltip("form_persist_tooltip") ?>
+            </label>
+
+        </li>
+        <?php
+    }
+}
+
+//Action to inject supporting script to the form editor page
+add_action("gform_editor_js", "editor_script_persistency");
+
+function editor_script_persistency() {
+    ?>
+    <script type='text/javascript'>
+            
+        function SetFormPersistency(){
+            form.isPersistent = jQuery("#form_persist_value").is(":checked");
+        }
+            
+        jQuery("#form_persist_value").attr("checked", form.isPersistent);       
+    </script>
+    <?php
+}
+
+//Filter to add a new tooltip
+add_filter('gform_tooltips', 'add_persistency_tooltips');
+
+function add_persistency_tooltips($tooltips) {
+    $tooltips["form_persist_tooltip"] = "<h6>Persistency</h6>Check this box to make this form persistant";
+    return $tooltips;
+}
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Gravity Forms Data Persistence Add-On ===
+Contributors: asthait
+Tags: gravity, form, data, field, persistence, add-on, addon, plugin, plug-in, extension
+Requires at least: 2.9.2
+Tested up to: 3.4
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin makes your Gravity Forms data-persistent.
+
+== Description ==
+
+Consider this scenario:
+
+a) Your site uses multipage <a href="http://www.gravityforms.com/" target="_blank">Gravity Forms</a>.
+b) Your user logs in to the site and starts filling up a 5-step form.
+c) During the 3rd step, the user leaves without completely finishing the form.
+d) Some days later, the user comes back and logs in to see that his inputs are all gone!
+
+This happens because Gravity Forms by default does not save partially submitted forms.
+
+Our plugin resolves this issue. Simple!
+
+== Installation ==
+
+
+<h4>Installation</h4>
+
+1. Upload extracted folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Can you please help me to customize/extend Gravity Form? =
+
+Yah, sure.no third party knows better about Gravity Form than us.
+
+= Should I pay for this as we do for Gravity Form? =
+
+No, this is absolutely free add-on.
+
+= Do you have any client support? =
+
+Not a dedicated support, but we will try our best to reply you back.
+
+== Screenshots ==
+
+1. Just check the box from form settings->Advanced Tab
